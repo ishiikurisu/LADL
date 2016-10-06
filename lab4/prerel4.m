cf106274192368d93b473f4af450906326cee23a
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-10-06 10:58:01 -0300

    Writing second part of prerel4

--- lab4/prerel4.m
+++ lab4/prerel4.m
@@ -1,6 +1,8 @@
 %% main: Main calculations for prerel4
 function [out] = main()
 % Main calculations for prerel4
+
+% PART 1: Linear motion
 disp('# 4.2. Linear motion');
 disp('## 4.2.1 Theoretical model');
 
@@ -14,14 +16,43 @@ fprintf('K = %.5f\n', K);
 
 % Declaring transfer functions
 fprintf('G = \n');
-gn = [a, 0, 0];
+gn = [0, 0, a];
 gd = [b, c, 0];
 G = tf(gn, gd)
 fprintf('H = \n');
 hn = [K*a/b, 0, 0];
 hd = [1, c/b, K*a/b];
 H = tf(hn, hd)
+
+% Calculating Bode diagram
+fprintf('- Bode diagram\n')
+figure;
 grid on;
 bode(H);
+
+% Calculating system poles for later analysis
+disp('- Polynomial roots');
 xd = roots(gd)
 xh = roots(hd)
+
+% PART 2: Rotational motion
+disp('# 4.3 Rotational motion');
+disp('## 4.3.1 Theoretical model');
+
+% Declaring physical parameters
+K = 1.5286;
+t = 0.0254;
+fprintf('K = %.5f\n', K);
+fprintf('tau = %.5f\n', t);
+
+% Declaring transfer functions
+fprintf('O =');
+on = [0 K];
+od = [t 1];
+O = tf(on, od)
+
+% Calculating Bode diagram
+fprintf('- Bode diagram\n');
+figure;
+grid on;
+bode(O);
